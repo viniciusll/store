5761aa95be0e34141bb1f2a0cb321fa306a2deaf
viniciusll <viniciuslemos179@gmai.com>
2022-03-10 22:34:38 -0300

    link to navbar

--- src/presetion/components/header/index.tsx
+++ src/presetion/components/header/index.tsx
@@ -1,21 +1,24 @@
+import { Link } from 'react-router-dom';
+
 import * as Styled from './styles';
 
 import { BsFillCartFill } from 'react-icons/bs';
 import Logo from '../../../assets/logo.png';
 
 export function Header() {
+  
   return (
     <Styled.HeaderContainer>
-      <a href="/"><img className="logo" src={Logo} alt="Logo company" /></a>
+      <Link to="/"><img className="logo" src={Logo} alt="Logo company" /></Link>
       <nav className='navigation'>
           <ul>
             <li>
-                <a href="/products">Produtos</a>
+                <Link to="/products">Produtos</Link>
             </li>
             <li>
-                <a className='button' href="/cart">
+                <Link className='button' to="/cart">
                   <BsFillCartFill />
-                </a>
+                </Link>
             </li>
           </ul>
       </nav>
